dd5438556f9052bdc29e218bdd267b383fa9b8b9
RefDev <62316890+RefDev-00@users.noreply.github.com>
2023-07-05 11:13:55 +0700

    perbaikan abstract class

--- abstractClass.php
+++ abstractClass.php
@@ -1,16 +1,19 @@
 <?php
 // Abstract Class -> class tersebut tidak bisa digunakan object secara langsung
 
-use Data\City;
-use Data\Country;
-use Data\Province;
+use Data\{Location, City, Province, Country};
 
 require_once "data/Location.php";
 
 // tidak bisa membuat class parent menjadi objek secara langsung harus menggunakan child class sebagai objek
 $location = new City();
-$location->name = 'Hello Refsi';
-var_dump($location);
+$location->name = 'Setia Budi';
+echo "I live in $location->name" . PHP_EOL;
 
-$location = new Province();
-$location = new Country();
+$locationProvince = new Province();
+$locationProvince->name = "Jakarta";
+echo "From $locationProvince->name" . PHP_EOL;
+
+$locationCountry = new Country();
+$locationCountry->name = "Indonesia";
+echo "Stay in $locationCountry->name" . PHP_EOL;
